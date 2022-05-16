8e7ac616c97171e3dbbba15ddf23e9f932693d9d
Steven Laczko <stevendlaczko@gmail.com>
2022-05-16 11:49:33 -0400

    Added vimliterc and auto pluginstall

--- install.sh
+++ install.sh
@@ -3,16 +3,21 @@ if [[ "$(pwd)" != *vim-install ]]; then
     cd vim-install
 fi
 
-cp "$(pwd)/vimrc" "$HOME/.vimrc"
+if [[ $* =~ *--lite-vimrc* ]]; then
+    cp "$(pwd)/vimliterc" "$HOME/.vimrc"
+else
+    cp "$(pwd)/vimrc" "$HOME/.vimrc"
+fi
 mkdir -p "$HOME/.vim/pack/my-plugins/start"
 curl -fLo "$HOME/.vim/autoload/plug.vim" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-if [[ $1 != "--no-mdcat" ]]; then
+if [[ ! $* =~ *--no-mdcat* ]]; then
     git clone https://codeberg.org/flausch/mdcat.git
     apt install cargo
     cd ./mdcat
     cargo install --path .
+    echo "Use ,m to preview a markdown file."
 fi
+vim -c 'PlugInstall --sync' +qa
 echo "Type :NERDtree for help with that."
 echo "Type :h fzf for help with fzf."
-echo "Use ,m to preview a markdown file."
-echo "Now call :PlugInstall in Vim and you're done!"
+echo "Annnd you're done!"
